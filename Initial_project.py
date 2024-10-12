d7a933fad4866fbf411294b0a8c645634976a817
Remo <remo.brands@kpnmail.nl>
2024-10-12 16:22:17 +0000

    filtered data

--- Initial_project.py
+++ Initial_project.py
@@ -1,5 +1,24 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 data = pd.read_csv("volcano-events-2024-10-12_16-24-07_+0200.tsv", sep='\t')
 print(data.head())
+
+plt.plot(data['Year'], data['Deaths'])
+plt.xlabel('Year')
+plt.ylabel('Deaths')
+plt.title('Volcanic Eruption Deaths Over Time')
+plt.show()
+
+def filter_data_by_year(dataframe, min_year):
+    return dataframe[dataframe['Year'] >= min_year]
+
+data_filtered = filter_data_by_year(data, 1630)
+print(data_filtered.head())
+
+plt.plot(data_filtered['Year'], data_filtered['Deaths'])
+plt.xlabel('Year')
+plt.ylabel('Deaths')
+plt.title('Volcanic Eruption Deaths Over Time')
+plt.show()
