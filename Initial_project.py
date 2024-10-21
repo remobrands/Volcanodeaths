a795c44ed12ba1f40e564aed475b103c30a2900e
Remo <remo.brands@kpnmail.nl>
2024-10-21 09:20:41 +0000

    data changed to decades

--- Initial_project.py
+++ Initial_project.py
@@ -22,3 +22,20 @@ plt.xlabel('Year')
 plt.ylabel('Deaths')
 plt.title('Volcanic Eruption Deaths Over Time')
 plt.show()
+
+print("Missing values per column (filtered data):\n", data_filtered.isnull().sum())
+data_cleaned = data_filtered.drop_duplicates()
+
+def aggregate_deaths_by_decade(dataframe):
+    dataframe['Decade'] = (dataframe['Year'] // 10) * 10
+    data_by_decade = dataframe.groupby('Decade').agg({'Deaths': 'sum'}).reset_index()
+    return data_by_decade
+
+data_by_decade = aggregate_deaths_by_decade(data_cleaned)
+print(data_by_decade)
+
+plt.plot(data_by_decade['Decade'], data_by_decade['Deaths'], marker='o')
+plt.xlabel('Decade')
+plt.ylabel('Deaths')
+plt.title('Volcanic Eruption Deaths Over Time (Aggregated by Decade)')
+plt.show()
